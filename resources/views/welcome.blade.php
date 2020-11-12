bcc7ddf9bdfdd65fc9b90f4a51a403f311efe66a
joe.yabuki <s.j.yabuki@gmail.com>
2020-11-12 09:24:07 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,11 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
             <img src="image/welcom.jpg" alt="トップページ画像">
             <h1>Have a good surf!!</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary' ]) !!}
         </div>
     </div>
     
